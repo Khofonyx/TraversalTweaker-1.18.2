9c4946fbe46da9714781fae2dd452c54077c0f6d
Khofonyx <thsaw21@gmail.com>
2023-07-11 11:50:31 -0400

    Test Update

--- src/main/java/com/khofo/traversaltweaker/item/ModItems.java
+++ src/main/java/com/khofo/traversaltweaker/item/ModItems.java
@@ -1,7 +1,6 @@
 package com.khofo.traversaltweaker.item;
 
 import com.khofo.traversaltweaker.traversaltweaker;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
